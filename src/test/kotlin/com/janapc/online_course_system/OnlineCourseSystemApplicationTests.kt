d789977147c72aac68d0dab32be34ff4874e4a63
janapc <janapc@janapcs-MacBook-Air.local>
2026-08-13 20:42:11 -0300

    fix(test): add ActiveProfiles('test') to OnlineCourseSystemApplicationTests

--- src/test/kotlin/com/janapc/online_course_system/OnlineCourseSystemApplicationTests.kt
+++ src/test/kotlin/com/janapc/online_course_system/OnlineCourseSystemApplicationTests.kt
@@ -2,12 +2,13 @@ package com.janapc.online_course_system
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test") // 👈 Adicione esta anotação aqui!
 class OnlineCourseSystemApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
