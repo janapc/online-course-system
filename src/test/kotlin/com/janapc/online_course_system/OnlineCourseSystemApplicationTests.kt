5a1e5cb9c8b5b47af92a3bf42b6f479e5c4bea17
janapc <janapc@janapcs-MacBook-Air.local>
2026-08-13 20:43:16 -0300

    fix(test): add ActiveProfiles('test') to OnlineCourseSystemApplicationTests

--- src/test/kotlin/com/janapc/online_course_system/OnlineCourseSystemApplicationTests.kt
+++ src/test/kotlin/com/janapc/online_course_system/OnlineCourseSystemApplicationTests.kt
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
-@ActiveProfiles("test") // 👈 Adicione esta anotação aqui!
+@ActiveProfiles("test")
 class OnlineCourseSystemApplicationTests {
 
     @Test
